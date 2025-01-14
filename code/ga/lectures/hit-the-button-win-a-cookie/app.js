e89607d4b6a02637185e6e1fec583a8dd0bb25b9
sara jalal <sarajail@hotmail.com>
2025-01-14 19:18:44 +0300

    Initial commit: Completed Hit the Button, Win a Cookie project

---
+++ code/ga/lectures/hit-the-button-win-a-cookie/app.js
@@ -0,0 +1,6 @@
+document.getElementById('cookie-button').addEventListener('click', function() {
+    const message = document.getElementById('message');
+    messageElement.textContent = "You won a cookie!";
+    message.style.display = 'block';
+  });
+  
